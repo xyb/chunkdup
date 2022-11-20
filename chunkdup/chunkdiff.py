2ff6301d0fa01991b49832d6be8a13b85644ad28
Xie Yanbo <xieyanbo@gmail.com>
2022-11-20 20:48:06 +0800

    code complexity

--- chunkdup/chunkdiff.py
+++ chunkdup/chunkdiff.py
@@ -53,6 +53,31 @@ def find_diff(chunks1, sizes1, chunks2, sizes2):
     return total, diff
 
 
+def fill_line(bar_size, total, diff):
+    zoom = bar_size / total
+
+    def char_bar(char, size, line):
+        width = ceil(size * zoom)
+        if width:
+            line.append(char * width)
+        return width
+
+    def padding_bar(width, max_width, line):
+        padding = max_width - width
+        if padding:
+            line.append(" " * padding)
+
+    line1 = []
+    line2 = []
+    for char1, char2, size1, size2 in diff:
+        width1 = char_bar(char1, size1, line1)
+        width2 = char_bar(char2, size2, line2)
+        width = max(width1, width2)
+        padding_bar(width1, width, line1)
+        padding_bar(width2, width, line2)
+    return line1, line2
+
+
 def print_diff(
     chunksums_file1,
     chunksums_file2,
@@ -64,14 +89,14 @@ def print_diff(
     """
     >>> import tempfile
     >>> f1 = tempfile.NamedTemporaryFile()
-    >>> _ = f1.write(b'sum1  ./a  fck0sha2!a:10,b:10,c:10,r:5,s:10\\n')
+    >>> _ = f1.write(b'sum1  ./a  fck0sha2!a:10,b:10,c:10,r:5,s:5,t:5\\n')
     >>> f1.flush()
     >>> f2 = tempfile.NamedTemporaryFile()
-    >>> _ = f2.write(b'sum2  ./b  fck0sha2!b:10,c:10,m:10,x:5,s:5,y:5\\n')
+    >>> _ = f2.write(b'sum2  ./b  fck0sha2!b:10,c:10,m:5,r:5,n:5,s:5,z:5\\n')
     >>> f2.flush()
     >>> print_diff(open(f1.name), open(f2.name), './a', './b', color=False)
-            45  -------==============----      =======
-            45         ==============++++++++++=======++++
+            45  --------===============    ====    ====----
+            45          ===============++++====++++====++++
     """
     (chunks1, sizes1), (chunks2, sizes2) = get_info(
         chunksums_file1,
@@ -82,23 +107,7 @@ def print_diff(
 
     total, diff = find_diff(chunks1, sizes1, chunks2, sizes2)
 
-    zoom = bar_size / total
-    line1 = []
-    line2 = []
-    for char1, char2, size1, size2 in diff:
-        width1 = ceil(size1 * zoom)
-        width2 = ceil(size2 * zoom)
-        if width1:
-            line1.append(char1 * width1)
-        if width2:
-            line2.append(char2 * width2)
-        width = max(width1, width2)
-        padding1 = width - width1
-        padding2 = width - width2
-        if padding1:
-            line1.append(" " * padding1)
-        if padding2:
-            line2.append(" " * padding2)
+    line1, line2 = fill_line(bar_size, total, diff)
 
     def colorful(line):
         colors = {
@@ -123,7 +132,7 @@ def main():
     """
     >>> import tempfile
     >>> f1 = tempfile.NamedTemporaryFile()
-    >>> _ = f1.write(b'sum1  ./a  fck0sha2!a:10,b:10,c:10,r:5,s:10\\n')
+    >>> _ = f1.write(b'sum1  ./a  fck0sha2!a:10,b:10,c:10,r:5,s:5,t:5\\n')
     >>> f1.flush()
     >>> f2 = tempfile.NamedTemporaryFile()
     >>> _ = f2.write(b'sum2  ./b  fck0sha2!b:10,c:10,m:10,x:5,s:5,y:5\\n')
