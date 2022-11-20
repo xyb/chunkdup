590e0e59a6c9e2701228a9a96a1871f3710ce883
Xie Yanbo <xieyanbo@gmail.com>
2022-11-21 00:38:30 +0800

    add one line diff bar (#15)
    * refactory
    
    * add one line diff bar
    
    * colorful one line diff bar

--- chunkdup/chunkdiff.py
+++ chunkdup/chunkdiff.py
@@ -1,35 +1,28 @@
 import sys
 from difflib import SequenceMatcher
+from itertools import groupby
 from math import ceil
 
-from chunksum.parser import parse_chunksums
-
-from .chunkdup import CheckSumIndex
+from .index import get_index
 
 
-GREY = "\033[100m"
-RED = "\033[101m"
-GREEN = "\033[102m"
-YELLOW = "\033[103m"
+GREY = "\033[90m"
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+GREY_BG = "\033[100m"
+RED_BG = "\033[101m"
+GREEN_BG = "\033[102m"
+YELLOW_BG = "\033[103m"
 END = "\033[0m"
 
 
-def get_info(chunksums_file1, chunksums_file2, path1, path2):
-    sums1 = parse_chunksums(chunksums_file1)
-    sums2 = parse_chunksums(chunksums_file2)
-    index1 = CheckSumIndex(sums1)
-    index2 = CheckSumIndex(sums2)
-
-    id1 = index1._files.get(path1).get("id")
-    id2 = index2._files.get(path2).get("id")
-
-    chunks1 = index1.file_id2chunk[id1]
-    chunks2 = index2.file_id2chunk[id2]
-
-    sizes1 = [index1.chunk2size.get(id) for id in chunks1]
-    sizes2 = [index2.chunk2size.get(id) for id in chunks2]
-
-    return (chunks1, sizes1), (chunks2, sizes2)
+def get_info(chunksums_file, path):
+    index = get_index(chunksums_file)
+    id = index._files.get(path).get("id")
+    chunks = index.file_id2chunk[id]
+    sizes = [index.chunk2size.get(id) for id in chunks]
+    return chunks, sizes
 
 
 def find_diff(chunks1, sizes1, chunks2, sizes2):
@@ -76,43 +69,44 @@ def fill_line(bar_size, total, diff):
     return line1, line2
 
 
-def print_diff(
-    chunksums_file1,
-    chunksums_file2,
-    path1,
-    path2,
+def get_bar_layer(chunksums_file1, chunksums_file2, path1, path2, bar_size=40):
+    chunks1, sizes1 = get_info(chunksums_file1, path1)
+    chunks2, sizes2 = get_info(chunksums_file2, path2)
+
+    total, diff = find_diff(chunks1, sizes1, chunks2, sizes2)
+    filesize1 = sum(sizes1)
+    filesize2 = sum(sizes2)
+    line1, line2 = fill_line(bar_size, total, diff)
+    return line1, line2, filesize1, filesize2
+
+
+def print_2lines_bar(
+    line1,
+    line2,
+    filesize1,
+    filesize2,
+    output=None,
     bar_size=40,
     color=True,
 ):
     """
-    >>> import tempfile
-    >>> f1 = tempfile.NamedTemporaryFile()
-    >>> _ = f1.write(b'sum1  ./a  fck0sha2!a:10,b:10,c:10,r:5,s:5,t:5\\n')
-    >>> f1.flush()
-    >>> f2 = tempfile.NamedTemporaryFile()
-    >>> _ = f2.write(b'sum2  ./b  fck0sha2!b:10,c:10,m:5,r:5,n:5,s:5,z:5\\n')
-    >>> f2.flush()
-    >>> print_diff(open(f1.name), open(f2.name), './a', './b', color=False)
-            45  --------===============    ====    ====----
-            45          ===============++++====++++====++++
+    >>> line1 = ['-----', '==', '-----', '===']
+    >>> line2 = ['++', '   ', '==', '+', '    ', '===']
+    >>> print_2lines_bar(line1, line2, 100, 70, color=False)
+           100  -----==-----===
+            70  ++   ==+    ===
+    >>> print_2lines_bar(line1, line2, 100, 70)
+    ... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+           100  ...
+            70  ...
     """
-    (chunks1, sizes1), (chunks2, sizes2) = get_info(
-        chunksums_file1,
-        chunksums_file2,
-        path1,
-        path2,
-    )
-
-    total, diff = find_diff(chunks1, sizes1, chunks2, sizes2)
-
-    line1, line2 = fill_line(bar_size, total, diff)
 
     def colorful(line):
         colors = {
-            "=": GREY,
-            "-": RED,
-            "+": GREEN,
-            " ": YELLOW,
+            "=": GREY_BG,
+            "-": RED_BG,
+            "+": GREEN_BG,
+            " ": YELLOW_BG,
         }
         return [colors[s[0]] + s + END for s in line]
 
@@ -120,10 +114,114 @@ def print_diff(
         line1 = colorful(line1)
         line2 = colorful(line2)
 
-    filesize1 = sum(sizes1)
-    filesize2 = sum(sizes2)
-    print("{:>10d}  {}".format(filesize1, "".join(line1)))
-    print("{:>10d}  {}".format(filesize2, "".join(line2)))
+    for size, line in ((filesize1, line1), (filesize2, line2)):
+        print(
+            "{:>10}  {}".format(size, "".join(line)),
+            file=output or sys.stdout,
+            flush=True,
+        )
+
+
+def print_1line_bar(
+    line1,
+    line2,
+    filesize1,
+    filesize2,
+    output=None,
+    bar_size=40,
+    color=True,
+):
+    """
+    >>> line1 = ['-----', '==', '     ', '===']
+    >>> line2 = ['++', '   ', '==', '+++++', '===']
+    >>> print_1line_bar(line1, line2, 100, 70, color=False)
+    ▀100  ▄70  ██▀▀▀▒▒▄▄▄▄▄▒▒▒
+    >>> print_1line_bar(line1, line2, 100, 70, color=True)
+    ... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    ▀100  ▄70  ...
+    """
+
+    pairs = list("".join(x) for x in zip("".join(line1), "".join(line2)))
+    chars = {
+        "==": "▒",
+        "-+": "█",
+        "- ": "▀",
+        " +": "▄",
+    }
+    colors = {
+        "==": ["▒", GREY + GREY_BG],  # fg: grey, bg: grey
+        "-+": ["▀", RED + GREEN_BG],  # fg/top half: red, bg/bottom half: green
+        "- ": ["▀", RED + YELLOW_BG],  # fg: red, bg: yellow
+        " +": ["▀", YELLOW + GREEN_BG],  # fg: yellow, bg: green
+    }
+    bar = []
+    for key, group in groupby(pairs):
+        width = len(list(group))
+        if color:
+            char, color_ = colors[key]
+            item = color_ + char * width + END
+        else:
+            item = chars[key] * width
+        bar.append(item)
+
+    print(
+        "▀{}  ▄{}  {}".format(
+            filesize1,
+            filesize2,
+            "".join(bar),
+            file=output,
+            flush=True,
+        ),
+    )
+
+
+def print_diff(
+    chunksums_file1,
+    chunksums_file2,
+    path1,
+    path2,
+    output=None,
+    bar_size=40,
+    color=True,
+    oneline=True,
+):
+    """
+    >>> import sys
+    >>> import tempfile
+    >>> f1 = tempfile.NamedTemporaryFile()
+    >>> _ = f1.write(b'sum1  ./a  fck0sha2!a:10,b:10,c:10,r:5,s:5,t:5\\n')
+    >>> f1.flush()
+    >>> f2 = tempfile.NamedTemporaryFile()
+    >>> _ = f2.write(b'sum2  ./b  fck0sha2!b:10,c:10,m:5,r:5,n:5,s:5,z:5\\n')
+    >>> f2.flush()
+    >>> a, b = open(f1.name), open(f2.name)
+    >>> print_diff(a, b, './a', './b', color=False)
+    ▀45  ▄45  ▀▀▀▀▀▀▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▄▄▄▄▒▒▒▒████
+    >>> a, b = open(f1.name), open(f2.name)
+    >>> print_diff(a, b, './a', './b', color=False, oneline=False)
+            45  --------===============    ====    ====----
+            45          ===============++++====++++====++++
+    """
+
+    line1, line2, filesize1, filesize2 = get_bar_layer(
+        chunksums_file1,
+        chunksums_file2,
+        path1,
+        path2,
+    )
+    if oneline:
+        print_func = print_1line_bar
+    else:
+        print_func = print_2lines_bar
+    print_func(
+        line1,
+        line2,
+        filesize1,
+        filesize2,
+        output=output or sys.stdout,
+        bar_size=bar_size,
+        color=color,
+    )
 
 
 def main():
@@ -135,14 +233,22 @@ def main():
     >>> f2 = tempfile.NamedTemporaryFile()
     >>> _ = f2.write(b'sum2  ./b  fck0sha2!b:10,c:10,m:10,x:5,s:5,y:5\\n')
     >>> f2.flush()
-    >>> sys.argv = ['chunkdiff', f1.name, f2.name, './a', './b']
+    >>> sys.argv = ['chunkdiff', f1.name, f2.name, './a', './b', 'nocolor']
     >>> main()  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-                45  ...
-                45  ...
+    ▀45  ▄45  ▀▀▀▀▀▀▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████▄▄▄▄▄▄▄▒▒▒▒████
     """
-    if len(sys.argv) == 5:
+    if len(sys.argv) >= 5:
         sums_path1, sums_path2, path1, path2 = sys.argv[1:5]
-        print_diff(open(sums_path1), open(sums_path2), path1, path2)
+        color = True
+        if len(sys.argv) >= 6:
+            color = sys.argv[5] != "nocolor"
+        print_diff(
+            open(sums_path1),
+            open(sums_path2),
+            path1,
+            path2,
+            color=color,
+        )
     else:
         pass
 
